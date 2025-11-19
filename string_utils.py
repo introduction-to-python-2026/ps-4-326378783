8e370c637f5cd341989001b717b1bb5067c51fe5
Bar johan <barjohan25.11@gmail.com>
2025-11-19 19:38:18 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -2,21 +2,15 @@ def split_before_each_uppercases(formula):
     start = 0
     end = 1
     elements_lst = []
-    
     if not formula:
         return elements_lst
-    
     while end < len(formula):
         if formula[end].isupper():
             elements_lst.append(formula[start:end])
             start = end
-        end+=1  
-        
+        end+=1
     elements_lst.append(formula[start:])
-    
     return elements_lst
-
-
 def split_at_first_digit(formula):
     for char_index, char in enumerate(formula):
         if char.isdigit():
