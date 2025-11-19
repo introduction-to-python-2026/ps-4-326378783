5a9161e525b299c7cb2b384d04d5635fb4c24b36
Bar johan <barjohan25.11@gmail.com>
2025-11-19 19:33:13 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,17 +1,24 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    start = 0
+    end = 1
+    elements_lst = []
+    
+    if not formula:
+        return elements_lst
+    
+    while end < len(formula):
+        if formula[end].isupper():
+            elements_lst.append(formula[start:end])
+            start = end
+        end+=1  
+        
+    elements_lst.append(formula[start:])
+    
+    return elements_lst
 
 
-from string import digits
 def split_at_first_digit(formula):
-    digit_place = None
-    for idx , char in enumerate(formula):
+    for char_index, char in enumerate(formula):
         if char.isdigit():
-            digit_place = idx
-            prefix = formula[:digit_place]
-            numrical_part = int(formula[digit_place :])
-            break
-        else:
-            prefix = formula
-            numrical_part = 1
-    return (prefix , numrical_part)
+            return formula[:char_index], int(formula[char_index:])
+    return formula, 1
